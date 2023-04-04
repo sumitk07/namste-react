624a1a55f9a112b20b826c2a626e4806d448a089
Sumit <sumit@Sumits-MacBook-Pro.local>
2023-04-04 15:14:12 +0800

    01

---
+++ app.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
